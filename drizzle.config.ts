f65e2a348a1230c48d18db245c53079c0d720de8
Oleksandr Petryk <sachapos50@gmail.com>
2025-05-15 19:59:33 +0300

    update drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,11 @@
 import 'dotenv/config';
+
 import type { Config } from 'drizzle-kit';
 
 export default {
   dialect: 'postgresql',
   out: './drizzle',
-  schema: './src/shared/drizzle/schema',
+  schema: './src/shared/drizzle/schemas/index.ts',
   dbCredentials: {
     url: process.env.POSTGRES_DB_URI!,
   },
