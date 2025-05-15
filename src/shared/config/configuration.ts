52e0ed4ef9aaae058611f520691cfaa1ed9674db
Oleksandr Petryk <sachapos50@gmail.com>
2025-05-15 20:40:07 +0300

    fix configuration

--- src/shared/config/configuration.ts
+++ src/shared/config/configuration.ts
@@ -6,27 +6,27 @@ import { NodeEnvEnum } from '../enums/app.enum';
 export const EnvConfigZ = z.object({
   NODE_ENV: z.nativeEnum(NodeEnvEnum).default(NodeEnvEnum.prod),
 
-  PORT: z.number(),
+  PORT: z.number().transform((value) => +value),
 
   JWT_ACCESS_KEY_PRIVATE: z.string(),
   JWT_ACCESS_KEY_PUBLIC: z.string(),
-  JWT_ACCESS_TOKEN_EXPIRES_IN: z.number(),
+  JWT_ACCESS_TOKEN_EXPIRES_IN: z.number().transform((value) => +value),
   JWT_REFRESH_KEY_PRIVATE: z.string(),
   JWT_REFRESH_KEY_PUBLIC: z.string(),
-  JWT_REFRESH_TOKEN_EXPIRES_IN: z.number(),
+  JWT_REFRESH_TOKEN_EXPIRES_IN: z.number().transform((value) => +value),
 
   PROFILE_PICTURE_SIZE: z.number(),
 
   POSTGRES_DB_URI: z.string(),
 
   REDIS_HOST: z.string(),
-  REDIS_PORT: z.number(),
+  REDIS_PORT: z.number().transform((value) => +value),
 
   KAFKA_BROKERS: z.array(z.string()),
   KAFKA_FROM_BEGINNING: z.boolean().default(true),
 
   MINIO_ENDPOINT: z.string(),
-  MINIO_PORT: z.number(),
+  MINIO_PORT: z.number().transform((value) => +value),
   MINIO_ACCESS_KEY: z.string(),
   MINIO_SECRET_KEY: z.string(),
 });
@@ -36,7 +36,25 @@ export type EnvConfig = z.infer<typeof EnvConfigZ>;
 export default (): EnvConfig => {
   const logger = new Logger('EnvConfig');
 
-  const envConfig = EnvConfigZ.parse(process.env);
+  console.log(process.env, '<<< env');
+
+  const envConfig = EnvConfigZ.parse({
+    ...process.env,
+
+    PORT: +process.env.PORT!,
+
+    JWT_ACCESS_TOKEN_EXPIRES_IN: +process.env.JWT_ACCESS_TOKEN_EXPIRES_IN!,
+    JWT_REFRESH_TOKEN_EXPIRES_IN: +process.env.JWT_REFRESH_TOKEN_EXPIRES_IN!,
+
+    PROFILE_PICTURE_SIZE: +process.env.PROFILE_PICTURE_SIZE!,
+
+    REDIS_PORT: +process.env.REDIS_PORT!,
+
+    KAFKA_BROKERS: process.env.KAFKA_BROKERS?.split(','),
+    KAFKA_FROM_BEGINNING: process.env.KAFKA_FROM_BEGINNING === 'true',
+
+    MINIO_PORT: +process.env.MINIO_PORT!,
+  });
 
   logger.log('Application configuration', envConfig);
 
