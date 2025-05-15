d3cba81f3a8fe50ee27ff4e7d2cc6f7bea3c7f68
Oleksandr Petryk <sachapos50@gmail.com>
2025-05-15 22:11:53 +0300

    update APP_OPENAPI_JSON_URL value

--- src/shared/const/app.ts
+++ src/shared/const/app.ts
@@ -1,4 +1,4 @@
 // URLs
 export const APP_GLOBAL_URL_PREFIX = 'api/liquidator';
 export const APP_SWAGGER_URL = `${APP_GLOBAL_URL_PREFIX}/docs`;
-export const APP_OPENAPI_JSON_URL = `${APP_GLOBAL_URL_PREFIX}/docs/openapi.json`;
+export const APP_OPENAPI_JSON_URL = `${APP_SWAGGER_URL}/openapi.json`;
