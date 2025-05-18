faaa194f7a20590db07ae228deecda1a560954af
Oleksandr Petryk <sachapos50@gmail.com>
2025-05-18 22:17:52 +0300

    fix schema path

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ import type { Config } from 'drizzle-kit';
 export default {
   dialect: 'postgresql',
   out: './drizzle',
-  schema: './src/shared/drizzle/schemas/index.ts',
+  schema: './src/shared/modules/drizzle/schemas/index.ts',
   dbCredentials: {
     url: process.env.POSTGRES_DB_URI!,
   },
