ca880a77a740adb4730833c2ce29be325e36107c
stanislav-petryk <stasps210@gmail.com>
2025-05-19 22:32:19 +0300

    add findByEmail metod to user dao

--- src/shared/dao/user.dto.ts
+++ src/shared/dao/user.dto.ts
@@ -1,7 +1,8 @@
 import { Inject, Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { eq } from 'drizzle-orm';
 
 import { Drizzle, DRIZZLE_CONNECTION } from '../modules/drizzle/drizzle.module';
-import { user } from '../modules/drizzle/schemas';
+import { user, UserSelectModel } from '../modules/drizzle/schemas';
 import { BaseDao } from './base.dto';
 
 @Injectable()
@@ -22,5 +23,26 @@ export class UserDao extends BaseDao<typeof user> {
 
   private notFound(message?: string): never {
     throw new NotFoundException(message || 'Contact not found');
+  }
+
+  public async findByEmail({
+    db = this.postgres,
+    email,
+  }: {
+    db?: Drizzle;
+    email: string;
+  }): Promise<UserSelectModel> {
+    try {
+      const find = await db.query.user.findFirst({
+        where: eq(user.email, email),
+      });
+
+      return find as UserSelectModel;
+    } catch (error) {
+      this.logger.error(
+        `Could not find ${this.options.entityName.plural}: ${error}`,
+      );
+      throw error;
+    }
   }
 }
