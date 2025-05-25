6cce03ab354ebf97f29924776f29c5e667f75aa4
Oleksandr Petryk <sachapos50@gmail.com>
2025-05-25 22:34:02 +0300

    chagne file permissions

--- scripts/ci/wait-for-docker-compose.sh
+++ scripts/ci/wait-for-docker-compose.sh
@@ -10,10 +10,12 @@ until nc -z localhost 29092; do
   echo "Waiting for Kafka..."
   sleep 2
 done
+
 # until curl -s http://localhost:9000/minio/health/live; do
 #   echo "Waiting for Minio..."
 #   sleep 2
 # done
+
 until curl -s localhost:8888/api/liquidator/health; do
   echo "Waiting for Liquidator..."
   sleep 2
