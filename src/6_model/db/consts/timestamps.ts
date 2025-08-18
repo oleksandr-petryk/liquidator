28f7821bcdc660523847fc49f7d40d8688e76a3c
stanislav-petryk <stasps210@gmail.com>
2025-08-18 18:42:26 +0300

    fix migration problem with timestamps and copypaste in client-fingerprint

--- src/6_model/db/consts/timestamps.ts
+++ src/6_model/db/consts/timestamps.ts
@@ -1,10 +1,9 @@
-import { sql } from 'drizzle-orm';
 import * as t from 'drizzle-orm/pg-core';
 
 export const drizzleTimestamps = {
   createdAt: t
-    .timestamp('created_at')
-    .default(sql`CURRENT_TIMESTAMP AT TIME ZONE 'UTC'`)
+    .timestamp('created_at', { withTimezone: true })
+    .defaultNow()
     .notNull(),
-  updatedAt: t.timestamp('updated_at'),
+  updatedAt: t.timestamp('updated_at', { withTimezone: true }).defaultNow(),
 };
