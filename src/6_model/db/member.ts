f245041bd21e5cc0e3200b677748f77560902bc3
stanislav-petryk <stasps210@gmail.com>
2025-08-23 19:55:57 +0300

    create new tables

---
+++ src/6_model/db/member.ts
@@ -0,0 +1,46 @@
+import { relations } from 'drizzle-orm';
+import { pgTable, uniqueIndex, uuid } from 'drizzle-orm/pg-core';
+
+import { drizzlePrimaryKey } from './consts/primaryKey';
+import { drizzleTimestamps } from './consts/timestamps';
+import { organization } from './organization';
+import { role } from './role';
+import { user } from './user';
+
+export const member = pgTable(
+  'member',
+  {
+    ...drizzlePrimaryKey,
+    userId: uuid('user_id')
+      .references(() => user.id)
+      .notNull(),
+    organizationId: uuid('organization_id')
+      .references(() => organization.id)
+      .notNull(),
+    roleId: uuid('role_id')
+      .references(() => role.id)
+      .notNull(),
+    ...drizzleTimestamps,
+  },
+  (t) => ({
+    uniqueUserOrganization: uniqueIndex('unique_user_organization').on(
+      t.userId,
+      t.organizationId,
+    ),
+  }),
+);
+
+export const memberRelations = relations(member, ({ one }) => ({
+  user: one(user, {
+    fields: [member.userId],
+    references: [user.id],
+  }),
+  organization: one(organization, {
+    fields: [member.organizationId],
+    references: [organization.id],
+  }),
+  role: one(role, {
+    fields: [member.roleId],
+    references: [role.id],
+  }),
+}));
---
+++ src/6_model/db/role.ts
@@ -0,0 +1,21 @@
+import { relations } from 'drizzle-orm';
+import { jsonb, pgTable, uuid, varchar } from 'drizzle-orm/pg-core';
+
+import { drizzlePrimaryKey } from './consts/primaryKey';
+import { organization } from './organization';
+
+export const role = pgTable('role', {
+  ...drizzlePrimaryKey,
+  organizationId: uuid('organization_id')
+    .references(() => organization.id)
+    .notNull(),
+  name: varchar({ length: 30 }).notNull(),
+  permissions: jsonb(),
+});
+
+export const roleRelations = relations(role, ({ one }) => ({
+  organization: one(organization, {
+    fields: [role.organizationId],
+    references: [organization.id],
+  }),
+}));
