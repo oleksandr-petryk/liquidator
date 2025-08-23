96866ed429cd3d04a90acff2107830334de1dc2d
stanislav-petryk <stasps210@gmail.com>
2025-08-23 19:56:14 +0300

    create dao for new tables

--- src/3_components/dao/dao.module.ts
+++ src/3_components/dao/dao.module.ts
@@ -3,9 +3,13 @@ import { Module } from '@nestjs/common';
 import { AccountVerificationDao } from './account-verification.dao';
 import { ActivityLogDao } from './activity-log.dao';
 import { ClientFingerprintDao } from './client-fingerprint.dao';
+import { MemberDao } from './member.dao';
 import { OrganizationDao } from './organization.dao';
 import { PasswordResetRequestDao } from './password-reset-request.dao';
+import { PermissionDao } from './permission.dao';
 import { PictureDao } from './pictures.dao';
+import { RoleDao } from './role.dao';
+import { RoleToPermissionDao } from './role-to-permission.dao';
 import { SessionDao } from './session.dao';
 import { TeamDao } from './team.dao';
 import { TeamToUserDao } from './team-to-user.dao';
@@ -16,9 +20,13 @@ import { UserDao } from './user.dao';
     AccountVerificationDao,
     ActivityLogDao,
     ClientFingerprintDao,
+    MemberDao,
     OrganizationDao,
     PasswordResetRequestDao,
+    PermissionDao,
     PictureDao,
+    RoleDao,
+    RoleToPermissionDao,
     SessionDao,
     TeamDao,
     TeamToUserDao,
@@ -28,9 +36,13 @@ import { UserDao } from './user.dao';
     AccountVerificationDao,
     ActivityLogDao,
     ClientFingerprintDao,
+    MemberDao,
     OrganizationDao,
     PasswordResetRequestDao,
+    PermissionDao,
     PictureDao,
+    RoleDao,
+    RoleToPermissionDao,
     SessionDao,
     TeamDao,
     TeamToUserDao,
---
+++ src/3_components/dao/member.dao.ts
@@ -0,0 +1,36 @@
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import type { InferInsertModel, InferSelectModel } from 'drizzle-orm';
+
+import {
+  Drizzle,
+  DRIZZLE_CONNECTION,
+} from '../../4_low/drizzle/drizzle.module';
+import { member } from '../../6_model/db';
+import { BaseDao } from './base.dao';
+import { OrganizationSelectModel } from './organization.dao';
+import { RoleSelectModel } from './role.dao';
+import { UserSelectModel } from './user.dao';
+
+export type MemberInsertModel = InferInsertModel<typeof member>;
+export type MemberSelectModel = InferSelectModel<typeof member> & {
+  user: UserSelectModel | null;
+  organization: OrganizationSelectModel | null;
+  role: RoleSelectModel | null;
+};
+
+@Injectable()
+export class MemberDao extends BaseDao<typeof member> {
+  private readonly logger = new Logger(MemberDao.name);
+
+  constructor(
+    @Inject(DRIZZLE_CONNECTION)
+    private readonly postgresDatabase: Drizzle,
+  ) {
+    super(member, postgresDatabase, {
+      entityName: {
+        singular: 'member',
+        plural: 'member',
+      },
+    });
+  }
+}
