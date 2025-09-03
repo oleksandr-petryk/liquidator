c93f731b28e93382405a369985e6d38de8bc21c2
stanislav-petryk <stasps210@gmail.com>
2025-09-03 22:36:43 +0300

    add check for correlations

--- test/unit/utils/e2e.util.test.ts
+++ test/unit/utils/e2e.util.test.ts
@@ -42,47 +42,103 @@ class TestPhoneNumberDto {
   phoneNumber!: string;
 }
 
-test('random email generation', () => {
-  const email = randomEmail();
+describe('Random email generation', () => {
+  test('Random email generation', () => {
+    const email = randomEmail();
 
-  const dto = new TestEmailDto();
-  dto.email = email;
+    const dto = new TestEmailDto();
+    dto.email = email;
 
-  const errors = validateSync(dto);
+    const errors = validateSync(dto);
 
-  expect(errors.length).toBe(0);
+    expect(errors.length).toBe(0);
+  });
+
+  test('Check correlations', () => {
+    const email: string[] = [];
+
+    for (let i = 0; i < 100; i++) {
+      email[i] = randomEmail();
+    }
+
+    const check = new Set(email).size === email.length;
+
+    expect(check).toBe(true);
+  });
 });
 
-test('random username generation', () => {
-  const username = randomUsername();
+describe('Random username generation', () => {
+  test('random username generation', () => {
+    const username = randomUsername();
 
-  const dto = new TestUsernameDto();
-  dto.username = username;
+    const dto = new TestUsernameDto();
+    dto.username = username;
 
-  const errors = validateSync(dto);
+    const errors = validateSync(dto);
 
-  expect(errors.length).toBe(0);
+    expect(errors.length).toBe(0);
+  });
+
+  test('Check correlations', () => {
+    const username: string[] = [];
+
+    for (let i = 0; i < 100; i++) {
+      username[i] = randomUsername();
+    }
+
+    const check = new Set(username).size === username.length;
+
+    expect(check).toBe(true);
+  });
 });
 
-test('random username generation', () => {
-  const password = randomPassword();
+describe('Random username generation', () => {
+  test('random username generation', () => {
+    const password = randomPassword();
 
-  const dto = new TestPasswordDto();
-  dto.password = password;
+    const dto = new TestPasswordDto();
+    dto.password = password;
 
-  const errors = validateSync(dto);
+    const errors = validateSync(dto);
 
-  expect(errors.length).toBe(0);
+    expect(errors.length).toBe(0);
+  });
+
+  test('Check correlations', () => {
+    const password: string[] = [];
+
+    for (let i = 0; i < 100; i++) {
+      password[i] = randomPassword();
+    }
+
+    const check = new Set(password).size === password.length;
+
+    expect(check).toBe(true);
+  });
 });
 
-test('random phone number generation', () => {
-  const phoneNumber = randomPhoneNumber();
+describe('Random phone number generation', () => {
+  test('Check functional', () => {
+    const phoneNumber = randomPhoneNumber();
 
-  const dto = new TestPhoneNumberDto();
-  dto.phoneNumber = phoneNumber;
+    const dto = new TestPhoneNumberDto();
+    dto.phoneNumber = phoneNumber;
 
-  const errors = validateSync(dto);
-  console.log(errors);
+    const errors = validateSync(dto);
+    console.log(errors);
 
-  expect(errors.length).toBe(0);
+    expect(errors.length).toBe(0);
+  });
+
+  test('Check correlations', () => {
+    const phoneNumber: string[] = [];
+
+    for (let i = 0; i < 100; i++) {
+      phoneNumber[i] = randomPhoneNumber();
+    }
+
+    const check = new Set(phoneNumber).size === phoneNumber.length;
+
+    expect(check).toBe(true);
+  });
 });
